abdff0103241a7b03f0951b5d0f998385ffbfdaa
murital <91139522+murital@users.noreply.github.com>
2021-10-03 10:50:37 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -3,12 +3,12 @@ terraform {
   # Terraform version at the time of writing this post
   required_version = ">= 0.12.24"
 
-  backend "s3" {
-    bucket = "ami-cp-state"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+  #backend "s3" {
+   # bucket = "ami-cp-state"
+   # key    = "terraform.tfstate"
+    #region = "us-east-1"
+  #}
+#}
 
 
 ## Provider us-east-1
